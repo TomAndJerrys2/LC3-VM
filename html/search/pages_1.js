315cc89e2d2ac9769fcc408a40a9573639f0ad6a
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-16 20:59:06 +0000

    Clang-format && Clang-tidy + Doxygen just to learn how proper project workflows are built up

---
+++ html/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['machine_0',['LC3 Virtual Machine',['../md_README.html',1,'']]]
+];
---
+++ html/search/pages_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['virtual_20machine_0',['LC3 Virtual Machine',['../md_README.html',1,'']]]
+];
