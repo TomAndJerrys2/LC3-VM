bb32d21c66d1eec015d27ee2525d6664657c94bf
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-17 17:02:39 +0000

    Functions for Bitwise AND && Bitwise NOT

--- opcodes.c
+++ opcodes.c
@@ -1,5 +1,6 @@
 #include "memory.h"
 
+// adding to register values
 void OP_ADD_FUNC(void) {
 
 	// destination register - DR
@@ -26,6 +27,8 @@ void OP_ADD_FUNC(void) {
 	update_flags(reg0);
 }
 
+// load indirect function for grabbing a value
+// from memory via the data bus
 void OP_LDI_FUNC(void) {
 	
 	// destination register - DR
@@ -38,4 +41,44 @@ void OP_LDI_FUNC(void) {
 	// to return the final location
 	registers[reg0] = mem_read(mem_read(R_PC) + pc_offset);
 	update_flags(reg0);
-}	
+}
+
+// Bitwise AND
+void OP_AND_FUNC(void) {
+	
+	// destination register - DR
+	uint16_t reg0 = (input_str >> 9) & 0x7;
+
+	// first operand to compare from register
+	uint16_t reg1 = (input_str >> 6) & 0x7;
+
+	// Immediate mode flag checks
+	uint16_t imm_flag = (input_str >> 5) & 0x1;
+	if(imm_flag) {
+		uint16_t imm5 = SIGN_EXTEND(input_str & 0x1F, 5);
+		
+		// if immediate mode, compare reg1 with the pre-stored
+		// value in the 5 bit allocated space for immediate mode
+		registers[reg0] = registers[reg1] & imm5;
+	}
+	// register mode
+	else {
+		uint16_t reg2 = input_str & 0x7;
+		registers[reg0] = registers[reg1] & registers[reg2];
+	}
+
+	update_flags(reg0);
+}
+
+// Bitwise NOT
+void OP_NOT_FUNC() {
+	
+	// Destination Register
+	uint16_t reg0 = (input_str >> 9) & 0x7;
+	
+	// value operand is inversed to represent NOT
+	uint16_t reg1 = (input_str >> 6) & 0x7;
+
+	registers[reg1] = ~registers[reg1];
+	update_flags(reg0);
+}
