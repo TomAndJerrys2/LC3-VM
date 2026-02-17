d96a6274e606a638721efc140dae1f993dcccc37
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-17 16:29:21 +0000

    Logical AND

--- opcodes.c
+++ opcodes.c
@@ -10,8 +10,18 @@ void OP_ADD_FUNC(void) {
 
 	// check whether we are in immediate mode
 	uint16_t imm_flag = (input_str >> 5) & 0x1;
-
+	
+	// if in immediate mode, we expand the 5 bits and add
+	// the pre-stored value to a given register
 	if(imm_flag) {
 		uint16_t imm5 = SIGN_EXTEND(input_str & 0x1F, 5);
+		registers[reg0] = registers[reg1] + imm5;
 	}
+	// in register mode
+	else {
+		uint16_t reg2 = input_str & 0x7;
+		registers[reg0] = registers[reg1] + registers[reg2];	
+	}
+
+	update_flags(reg0);
 }
