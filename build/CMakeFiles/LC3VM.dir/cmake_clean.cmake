9a4e8253949f234ed7642a920d5e1c1806382eab
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-17 19:03:39 +0000

    Fixed CMake Erros

---
+++ build/CMakeFiles/LC3VM.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LC3VM.dir/main.c.o"
+  "CMakeFiles/LC3VM.dir/main.c.o.d"
+  "CMakeFiles/LC3VM.dir/opcodes.c.o"
+  "CMakeFiles/LC3VM.dir/opcodes.c.o.d"
+  "CMakeFiles/LC3VM.dir/trap.c.o"
+  "CMakeFiles/LC3VM.dir/trap.c.o.d"
+  "LC3VM"
+  "LC3VM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LC3VM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
